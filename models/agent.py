6c26c832e1ce33f4e2728d76b42a9f014bfc5217
mendi kuperman <mendikuperman@gmail.com>
2025-06-26 16:08:04 +0300

    created person class

---
+++ models/agent.py
@@ -0,0 +1,11 @@
+
+class Agent:
+
+    def __init__(self,codeName,realName,location,status,missionsCompleated):
+        self.codeNmae = codeName
+        self.realName = realName
+        self.location = location
+        self.status = status
+        self.missionsCompleated = missionsCompleated
+
+
