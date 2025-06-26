430994b5c904c3e69f1b67e54e5f158ff1a941d1
mendi kuperman <mendikuperman@gmail.com>
2025-06-26 14:55:32 +0300

    creatd basic UI menu

---
+++ console-ui/menu.py
@@ -0,0 +1,18 @@
+class Menu:
+
+    def start(self):
+
+        print("-------welcome to EAGLE AGENT system---------")
+        print()
+
+        chose=""
+
+        while chose!="5":
+            print("       what would you like to do?")
+            print("           1.to add new agent ")
+            print("           2.deleat agent  ")
+            print("           3.get agent by id")
+            print("           4 get all agent")
+
+            chose= input()
+
